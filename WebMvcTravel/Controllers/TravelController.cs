987c9a27e818f2e4bfd39967953b36b4b38ffb1d
8615910461621 <1925618905@qq.com>
2019-05-13 20:51:51 +0800

    添加项目文件。

---
+++ WebMvcTravel/Controllers/TravelController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace WebMvcTravel.Controllers
+{
+    public class TravelController : Controller
+    {
+        // GET: Travel
+        /// <summary>
+        /// 主页面
+        /// </summary>
+        /// <returns></returns>        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 结果列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Result_List()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 结果列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Result_Grid()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 常见问题页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Faq()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 静态页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Static_Page()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 博客
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 博客查看更多详情
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Blog_Single()
+        {
+            return View();
+        }
+        public ActionResult Payment()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 预定成功页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
