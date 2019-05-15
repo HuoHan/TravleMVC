bdbb897fd832c57bb94b16a11ba52ce5d2c8cc6f
甜甜 <@DESKTOP-GNQI271>
2019-05-15 14:11:32 +0800

    Revert "WTT"
    This reverts commit 420657c9bd130d8d9aec5592ff9ab50df84c14bb.

--- WebMvcTravel/Controllers/TravelController.cs
+++ WebMvcTravel/Controllers/TravelController.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebMvcTravel.Models;
 using Newtonsoft.Json;
-using PagedList.Mvc;
-using PagedList;
 
 namespace WebMvcTravel.Controllers
 {
@@ -25,25 +22,17 @@ namespace WebMvcTravel.Controllers
         /// 结果列表
         /// </summary>
         /// <returns></returns>
-        public ActionResult Result_List(int pageIndex=1)
+        public ActionResult Result_List()
         {
-            int pageSize = 2;
-            string result = HttpClientHelper.Sender("get", "api/Result_List", null);
-            List<Routese> list = JsonConvert.DeserializeObject<List<Routese>>(result);
-            IPagedList<Routese> list1 = list.ToPagedList<Routese>(pageIndex, pageSize);
-            return View(list1);
+            return View();
         }
         /// <summary>
         /// 结果列表
         /// </summary>
         /// <returns></returns>
-        public ActionResult Result_Grid(int pageIndex = 1)
+        public ActionResult Result_Grid()
         {
-            int pageSize = 2;
-            string result = HttpClientHelper.Sender("get", "api/Result_Grid", null);
-            List<Routese> list = JsonConvert.DeserializeObject<List<Routese>>(result);
-            IPagedList<Routese> list1 = list.ToPagedList<Routese>(pageIndex, pageSize);
-            return View(list1);
+            return View();
         }
         /// <summary>
         /// 常见问题页面
@@ -69,23 +58,17 @@ namespace WebMvcTravel.Controllers
         /// 博客
         /// </summary>
         /// <returns></returns>
-        public ActionResult Blog(int pageIndex=1)
+        public ActionResult Blog()
         {
-            int pageSize = 2;
-            string result = HttpClientHelper.Sender("get","api/Blogs", null);
-            List<Blogs> list = JsonConvert.DeserializeObject<List<Blogs>>(result);
-            IPagedList<Blogs> list1 = list.ToPagedList<Blogs>(pageIndex,pageSize);
-            return View(list1);            
+            return View();
         }
         /// <summary>
         /// 博客查看更多详情1
         /// </summary>
         /// <returns></returns>
-        public ActionResult Blog_Single(int id=0)
+        public ActionResult Blog_Single()
         {
-            string result = HttpClientHelper.Sender("get", "api/Blogs", null);
-            List<Blogs> list = JsonConvert.DeserializeObject<List<Blogs>>(result);
-            return View(list.Where(m =>m.BlogsId==id).ToList());
+            return View();
         }
         public ActionResult Payment()
         {
