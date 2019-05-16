134dce47b1dd10080f610d5c40bd9e56f7663253
8615910461621 <1925618905@qq.com>
2019-05-16 20:10:23 +0800

    HYK

--- WebMvcTravel/Controllers/TravelController.cs
+++ WebMvcTravel/Controllers/TravelController.cs
@@ -19,13 +19,40 @@ namespace WebMvcTravel.Controllers
         /// <returns></returns>        
         public ActionResult Index()
         {
-            return View();
+            //ShowAddress();
+            return View(GetMonthes());
         }
+        public List<Addresses> GetMonthes()
+        {
+            string str = HttpClientHelper.Sender("get", "api/ScenicAreas", null);
+            var item = JsonConvert.DeserializeObject<List<Addresses>>(str);
+            return item;
+        }
+        public JsonResult Ajax(int id)
+        {
+            string str = HttpClientHelper.Sender("get", "api/ShowAddress?id=" + id, null);
+            var item = JsonConvert.DeserializeObject<List<Addresses>>(str);
+            return Json(item, JsonRequestBehavior.AllowGet);
+        }
+        //绑定月份
+        public JsonResult AjaxMonth()
+        {
+            string str = HttpClientHelper.Sender("get", "api/ShowMonth", null);
+            var item = JsonConvert.DeserializeObject<List<Monthes>>(str);
+            return Json(item, JsonRequestBehavior.AllowGet);
+        }
+        ////Json格式
+        //public string Ajax(int fid)
+        //{
+        //    string str = HttpClientHelper.Sender("get", "api/ShowAddress?id=" + fid, null);
+        //    var ai = JsonConvert.SerializeObject(str);
+        //    return ai;
+        //}
         /// <summary>
         /// 结果列表
         /// </summary>
         /// <returns></returns>
-        public ActionResult Result_List()
+        public ActionResult Result_List(int placeId)
         {
             return View();
         }
@@ -61,23 +88,23 @@ namespace WebMvcTravel.Controllers
         /// 博客
         /// </summary>
         /// <returns></returns>
-        public ActionResult Blog(int pageIndex=1)
+        public ActionResult Blog(int pageIndex = 1)
         {
             int pageSize = 2;
             string result = HttpClientHelper.Sender("get", "api/Blogs", null);
             List<Blogs> str = JsonConvert.DeserializeObject<List<Blogs>>(result);
-            IPagedList<Blogs> list = str.ToPagedList<Blogs>(pageIndex,pageSize);
+            IPagedList<Blogs> list = str.ToPagedList<Blogs>(pageIndex, pageSize);
             return View(list);
         }
         /// <summary>
         /// 博客查看更多详情1
         /// </summary>
         /// <returns></returns>
-        public ActionResult Blog_Single(int id=0)
+        public ActionResult Blog_Single(int id = 0)
         {
             string result = HttpClientHelper.Sender("get", "api/Blogs", null);
             List<Blogs> str = JsonConvert.DeserializeObject<List<Blogs>>(result);
-            return View(str.Where(m=>m.BlogsId==id).ToList());
+            return View(str.Where(m => m.BlogsId == id).ToList());
         }
         public ActionResult Payment()
         {
--- WebMvcTravel/Models/Addresses.cs
+++ WebMvcTravel/Models/Addresses.cs
@@ -13,6 +13,34 @@ namespace WebMvcTravel.Models
         public string AddressName { get; set; }
         public int Pid { get; set; }
         public int Month_Id { get; set; }
-        public List<Monthes> Monthes { get; set; }
+        public int CountryId { get; set; }
+        public int PlaceId { get; set; }
+        public string PlaceName { get; set; }
+
+
+
+        public int ScenicAreaId { get; set; }
+        public string ScenicAreaName { get; set; }
+        public int Url_Id { get; set; }
+        public string Introduce { get; set; }
+        public string ScenicAreaUrl { get; set; }
+
+
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AddId { get; set; }
+        public int ScId { get; set; }
+
+
+
+        public int RouteId { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public int StartArae { get; set; }
+        public int EndArae { get; set; }
+        public int Includes { get; set; }
+        public double Price { get; set; }
     }
 }
